03135ddf199d7dd024234dc70891e823ff3121ab
alex <alex.kudelko@gmail.com>
2022-09-30 14:50:55 +0300

    add check status

---
+++ status.py
@@ -0,0 +1,77 @@
+"""
+This script must be run on your Node
+    Example run script every 5 minutes:
+    Install to cron to run every 5(o) min (on Ubuntu example):
+    srcipt dir: /root/scripts
+    cd /root/scripts
+    chmod +x status.py
+    crontab -e
+    select nano (easiest way) if asked
+    add this line
+    */5 * * * * /usr/bin/python3 /root/scripts/status.py > /dev/null 2>&1
+    press ctrl+X and safe changes
+
+"""
+
+from os import system
+
+import requests
+
+IP = "127.0.0.1"
+"""
+IP = ip address your node or any other public node.
+If script run on node -  IP = "127.0.0.1" 
+"""
+
+WALLET_ADDRESS = ""
+MASSA_PASSWD = ""
+"""Massa wallet address and password"""
+
+apiToken = ""
+chatID = ""
+"""
+apiToken you can get with @BotFather when creating bot
+chatID you can get this way:
+1) Go to bot and send /start
+2) Send anything to bot
+3) In browser go https://api.telegram.org/bot_API_TOKEN_/getUpdates
+example "https://api.telegram.org/bot534238456:AAHbl_s-sr3FR6TxWGLeuxN5EfGQefwegasfgg/getUpdates"
+"""
+apiURL = f'https://api.telegram.org/bot{apiToken}/sendMessage'
+
+
+def send_to_telegram(message):
+    try:
+        response = requests.post(apiURL, json={'chat_id': chatID, 'text': message})
+    except Exception as e:
+        return e
+
+
+def get_response(json_data: dict):
+    try:
+        response = requests.post(f"http://{IP}:33035", json=json_data)
+    except requests.ConnectionError:
+        return None
+    return response
+
+
+if __name__ == '__main__':
+
+    json_data = {"id": "1", "jsonrpc": "2.0", "method": "get_addresses",
+                 "params": [["A127LPgaKbBVHov5Zs8Qu1TiYYAndC2qpJf15x2zD57q4ekYom4D"]]}
+
+    resp = get_response(json_data=json_data)
+    rolls = resp.json().get('result')[0].get('final_roll_count')
+    candidate_rolls = resp.json().get('result')[0].get('candidate_roll_count')
+    active_rolls = resp.json().get('result')[0].get('cycle_infos')[-1].get('active_rolls')
+    balance = float(resp.json().get('result')[0].get('final_sequential_balance'))
+
+    if active_rolls == 0 and candidate_rolls == 0:
+        send_to_telegram(f"Alert !!! Active Rolls: {active_rolls}")
+        if balance < 100:
+            send_to_telegram(f"Not enough balance {balance} , can't buy roll")
+        else:
+            system(
+                f"cd /root/massa/massa-client && ./massa-client -p {MASSA_PASSWD} buy_rolls {WALLET_ADDRESS} 1 0 >> /root/status.log")
+            send_to_telegram(f"Roll purchased successfully! \n Active Rolls: {active_rolls}\n Rolls: {rolls}\n "
+                             f"Candidate Rolls: {candidate_rolls}")
